d8150019b8f130a51874133f3863bc6f42f78dea
Topher Brown <topher200@gmail.com>
2011-02-23 02:16:39 -0500

    adding clojure-contrib to the project requirements since we use it now

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject functional-koans "0.2.0"
   :description "The functional koans"
   :dependencies [[org.clojure/clojure "1.3.0-alpha3"]
-                 [jline "0.9.94"]])
+                 [jline "0.9.94"]
+                 [org.clojure/clojure-contrib "1.1.0"]])
